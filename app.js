f1ccb6f54d6aa1e3d1ba82591d9be0df535c0b54
jon <jon@jons-MacBook-Pro.local>
2019-05-06 07:12:19 -0400

    using import and let

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var webdriver = require('selenium-webdriver');
+import webdriver from 'selenium-webdriver'
 
 // Open Chrome Browser
 // var driver = new webdriver.Builder().forBrowser('chrome').build();
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+let driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 // Open google.com
 driver.get('http://www.google.com');
